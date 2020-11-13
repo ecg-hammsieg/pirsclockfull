5643a408e21086cc40f549180cc3b02d5a1ec0e0
Benjamin Wiens <ich@benwiens.de>
2020-11-13 22:04:13 +0100

    GPIOs entfernen

--- pirsclockfull.py
+++ pirsclockfull.py
@@ -1,15 +1,8 @@
 #! /usr/bin/env python
-import pygame , sys , math, time, os
-import RPi.GPIO as GPIO
+import pygame, sys, math, time, os
+#import RPi.GPIO as GPIO
 from pygame.locals import *
-os.environ['SDL_VIDEODRIVER']="fbcon"
-
-# Setting up the GPIO and inputs with pull up
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN, GPIO.PUD_UP)
-GPIO.setup(12, GPIO.IN, GPIO.PUD_UP)
-GPIO.setup(13, GPIO.IN, GPIO.PUD_UP)
-GPIO.setup(15, GPIO.IN, GPIO.PUD_UP)
+#os.environ['SDL_VIDEODRIVER']="fbcon"
 
 pygame.init()
 bg = pygame.display.set_mode()
@@ -19,31 +12,25 @@ pygame.mouse.set_visible(False)
 # Change colour to preference (R,G,B) 255 max value
 bgcolour       = (0,   0,   0  )
 clockcolour    = (255, 255, 255)
-ind1colour     = (255, 0,   0  )
-ind2colour     = (255, 255, 0  )
-ind3colour     = (0,   255, 0  )
-ind4colour     = (0,   255, 255)
+ind1colour     = (255, 255, 255)
 offcolour      = (16,  16,  16 )
 
 # Scaling to the right size for the display
 digiclocksize  = int(bg.get_height()/3.5)
-digiclockspace = int(bg.get_height()/10.5)
+digiclockspace = int(bg.get_height()/12.5)
 dotsize        = int(bg.get_height()/90)
 hradius        = bg.get_height()/2.5
 secradius      = hradius - (bg.get_height()/26)
-indtxtsize     = int(bg.get_height()/5)
+indtxtsize     = int(bg.get_height()/10)
 indboxy        = int(bg.get_height()/6)
 indboxx        = int(bg.get_width()/2.5)
 
 # Coords of items on display
-xclockpos      = int(bg.get_width()*0.2875)
+xclockpos      = int(bg.get_width()*0.5)
 ycenter        = int(bg.get_height()/2)
-xtxtpos        = int(bg.get_width()*0.75)
+xtxtpos        = int(bg.get_width()*0.85)
 xindboxpos     = int(xtxtpos-(indboxx/2))
-ind1y          = int((ycenter*0.4)-(indboxy/2))       
-ind2y          = int((ycenter*0.8)-(indboxy/2))
-ind3y          = int((ycenter*1.2)-(indboxy/2))
-ind4y          = int((ycenter*1.6)-(indboxy/2))
+ind1y          = int((ycenter*1.2)-(indboxy/2))
 txthmy         = int(ycenter-digiclockspace)
 txtsecy        = int(ycenter+digiclockspace)
 
@@ -52,16 +39,10 @@ clockfont     = pygame.font.Font(None,digiclocksize)
 indfont       = pygame.font.Font(None,indtxtsize)
 
 # Indicator text - edit text in quotes to desired i.e. "MIC" will show MIC on display
-ind1txt       = indfont.render("MIC",True,bgcolour)
-ind2txt       = indfont.render("PHONE",True,bgcolour)
-ind3txt       = indfont.render("ON AIR",True,bgcolour)
-ind4txt       = indfont.render("DOOR",True,bgcolour)
+ind1txt       = indfont.render("ON AIR",True,ind1colour)
 
 # Indicator positions
-txtposind1 = ind1txt.get_rect(centerx=xtxtpos,centery=ycenter*0.4)
-txtposind2 = ind2txt.get_rect(centerx=xtxtpos,centery=ycenter*0.8)
-txtposind3 = ind3txt.get_rect(centerx=xtxtpos,centery=ycenter*1.2)
-txtposind4 = ind4txt.get_rect(centerx=xtxtpos,centery=ycenter*1.6)
+txtposind1 = ind1txt.get_rect(centerx=xtxtpos,centery=ycenter*0.2)
 
 # Parametric Equations of a Circle to get the markers
 # 90 Degree ofset to start at 0 seconds marker
@@ -117,33 +98,32 @@ while True :
     txtpossec     = digiclocksec.get_rect(centerx=xclockpos,centery=txtsecy)
 
     # Function for the indicators
-    if GPIO.input(11):
-        pygame.draw.rect(bg, offcolour,(xindboxpos, ind1y, indboxx, indboxy))
-    else:
-        pygame.draw.rect(bg, ind1colour,(xindboxpos, ind1y, indboxx, indboxy))
+    # if GPIO.input(11):
+         #pygame.draw.rect(bg, offcolour,(xindboxpos, ind1y, indboxx, indboxy))
+    # else:
+    #     pygame.draw.rect(bg, ind1colour,(xindboxpos, ind1y, indboxx, indboxy))
 
-    if GPIO.input(12):
-        pygame.draw.rect(bg, offcolour,(xindboxpos, ind2y, indboxx, indboxy))
-    else:
-        pygame.draw.rect(bg, ind2colour,(xindboxpos, ind2y, indboxx, indboxy))
+    # if GPIO.input(12):
+    #     pygame.draw.rect(bg, offcolour,(xindboxpos, ind2y, indboxx, indboxy))
+    # else:
+    #     pygame.draw.rect(bg, ind2colour,(xindboxpos, ind2y, indboxx, indboxy))
 
-    if GPIO.input(13):
-        pygame.draw.rect(bg, offcolour,(xindboxpos, ind3y, indboxx, indboxy))
-    else:
-        pygame.draw.rect(bg, ind3colour,(xindboxpos, ind3y, indboxx, indboxy))
+    # if GPIO.input(13):
+        #pygame.draw.rect(bg, offcolour,(xindboxpos, ind1y, indboxx, indboxy))
+    # else:
+        #pygame.draw.rect(bg, ind1colour,(xindboxpos, ind1y, indboxx, indboxy))
 
-    if GPIO.input(15):
-        pygame.draw.rect(bg, offcolour,(xindboxpos, ind4y, indboxx, indboxy))
-    else:
-        pygame.draw.rect(bg, ind4colour,(xindboxpos, ind4y, indboxx, indboxy))
+    # if GPIO.input(15):
+    #     pygame.draw.rect(bg, offcolour,(xindboxpos, ind4y, indboxx, indboxy))
+    # else:
+    #     pygame.draw.rect(bg, ind4colour,(xindboxpos, ind4y, indboxx, indboxy))
     
     # Render the text
     bg.blit(digiclockhm, txtposhm)
     bg.blit(digiclocksec, txtpossec)
-    bg.blit(ind1txt, txtposind1)
-    bg.blit(ind2txt, txtposind2)
-    bg.blit(ind3txt, txtposind3)
-    bg.blit(ind4txt, txtposind4)
+
+    #Aufgabe: Youtube Livestream implementieren
+    #bg.blit(ind1txt, txtposind1)
     
     time.sleep(0.04)
     pygame.time.Clock().tick(25)
